85c71122af6dbe0fefc86d1260e00fcbf34340e7
kostya <kostya.shefer.999@gmail.com>
2018-02-25 17:21:24 +0700

    Залив основоного файла

---
+++ lab2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf ("Введите первое число\n");
+    scanf ("%d", &a);
+    printf ("Введите второе число\n");
+    scanf ("%d", &b);
+    c = sum(a, b);
+    printf("Сумма чисел: %d \n", c);
+    return 0;
+}
